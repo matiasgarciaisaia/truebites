688b42e940dae66214ffccf0134b99eae16fe8da
Matias Garcia Isaia <mgarcia@manas.com.ar>
2016-10-20 19:04:09 -0300

    Users with name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+  validates_presence_of :name
 end
